46462f0966989d118244bade3f7c4e22837dab6d
Bhavnish Arora <arorabhavnish108@gmail.com>
2022-09-11 15:56:45 +0530

    Web app created

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import json
 import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
+from numpy.core.numeric import outer
 import pandas as pd
 
 
@@ -14,8 +16,9 @@ scaler = pickle.load(open('scaling.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -27,6 +30,18 @@ def predict():
     print(Output[0])
     return jsonify(Output[0])
 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+
+    input = np.array(data).reshape(1,-1)
+    final_input = scaler.transform(input)
+    print(final_input)
+
+    output = regmodel.predict(final_input)[0]
+
+    return render_template('home.html', prediction_text= "The Predicted price of the House is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
